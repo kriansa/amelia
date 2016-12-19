584400fa5520d19294e4e902f495b4e01af6e177
Daniel Pereira <zdaniel.gp@gmail.com>
2016-12-19 01:50:18 -0200

    Move layout common code to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def app_title
+    "Amelia"
+  end
+
+  def flash_messages
+    <<~HTML
+      <p class="notice">#{flash[:notice]}</p>
+      <p class="alert">#{flash[:alert]}</p>
+    HTML
+  end
+
 end
