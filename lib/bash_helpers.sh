694a9c9c773ca4f25e15e022c2d2b4a51e6d67f6
Daniel Pereira <zdaniel.gp@gmail.com>
2017-09-17 17:44:41 -0300

    Make shell scripts work with POSIX sh

--- lib/bash_helpers.sh
+++ lib/bash_helpers.sh
@@ -1,35 +1,34 @@
 # Returns a console-colored red string
-function red {
-  printf "\033[31m$1\033[0m\n"
+red() {
+  printf "\033[31m%s\033[0m\n" "$1"
 }
 
 # Returns a console-colored green string
-function green {
-  printf "\033[0;32m$1\033[0m\n"
+green() {
+  printf "\033[0;32m%s\033[0m\n" "$1"
 }
 
 # Returns a console-colored yellow string
-function yellow {
-  printf "\033[0;33m$1\033[0m\n"
+yellow() {
+  printf "\033[0;33m%s\033[0m\n" "$1"
 }
 
-function status {
+status() {
   yellow " ➤  $1"
 }
 
-function success {
+success() {
   green " ✔  $1"
 }
 
-function fail {
+fail() {
   red " ✖   $1"
   exit 1
 }
 
 # Execute the command and finish with error if it fails
-function execute {
-  $1
-  if [[ $? != 0 ]]; then
+execute() {
+  if $1; then
     echo
     red "Command '${1}' failed!"
     exit 1
