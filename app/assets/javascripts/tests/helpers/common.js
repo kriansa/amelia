dbf22af2728160fbe45245c9a779dec2f634cbe4
Daniel Pereira <zdaniel.gp@gmail.com>
2017-10-18 03:37:58 -0200

    Add docs on global test file

--- app/assets/javascripts/tests/helpers/common.js
+++ app/assets/javascripts/tests/helpers/common.js
@@ -1,3 +1,9 @@
+/**
+ * This file is globally included on every testcase instance.  It is useful to
+ * load constants such as `chai`, `sinon` or any setup phase that we need.  In
+ * our case, we need to setup sinon-chai, avoiding repeating a chunk of code
+ * across our test files.
+ */
 const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
